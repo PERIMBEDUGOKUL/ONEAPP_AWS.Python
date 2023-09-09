bcd4dfd8660c6a778f1186af06ec83cac2b4f230
PEREIMBEDUGOKUL <gokul.kote@gmail.com>
2023-09-09 11:32:27 +0530

    changes

--- oneapp/static/js/profile.js
+++ oneapp/static/js/profile.js
@@ -1,5 +1,5 @@
-$('.dy-btn').click(function(){
-    $('.sidebar .aws .dy-show').toggleClass("show"); 
+$('.dpy-btn').click(function(){
+    $('.sidebar .aws .dpy-show').toggleClass("show"); 
     $('.sidebar .aws .first').toggleClass("rotate");
   });
 // $('.sidebar .aws .aws-active').click(function(){
